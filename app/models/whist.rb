0106f53cba1bf2ef25cee42beae0236f7d53c92d
m0thman <dmitry@lebed.me>
2013-04-06 22:05:17 +0300

    Fix users

--- app/models/whist.rb
+++ app/models/whist.rb
@@ -1,6 +1,7 @@
 class Whist < ActiveRecord::Base
   # attr_accessible :title, :body
-  belongs_to :participation, :user
+  belongs_to :participation
+  belongs_to :user
 
   validates_presence_of :participation, :user
 end
